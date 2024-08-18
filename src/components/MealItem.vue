9feee30705c3bc549a314502ab104d24c1a63724
ilhamizzul <ilhamizzul@gmail.com>
2024-08-18 15:21:55 +0700

    create meal list component

---
+++ src/components/MealItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="bg-white shadow rounded-xl">
+				<router-link :to="{name: 'mealDetails', params: {id: meal.idMeal}}">
+					<img :src="meal.strMealThumb" :alt="meal.strMeal" class="rounded-t-xl h-48 w-full object-cover">
+				</router-link>
+				<div class="p-3">
+					<h3 class="font-semibold">{{ meal.strMeal }}</h3>
+					<p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem cumque placeat illum alias blanditiis possimus.</p>
+					<div class="flex justify-between items-center">
+						<YouTubeButton :href="meal.strYoutube">Youtube</YouTubeButton>
+					</div>
+				</div>
+			</div>
+</template>
+
+<script setup>
+  import YouTubeButton from '../components/YouTubeButton.vue';
+  const { meal } = defineProps({
+    meal: Object
+  })
+</script>
--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -9,40 +9,28 @@
 				/>
 		</div>
 		<div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
-			<div v-for="meal of meals" :key="meal.idMeal" class="bg-white shadow rounded-xl">
-				<router-link :to="{name: 'mealDetails', params: {id: meal.idMeal}}">
-					<img :src="meal.strMealThumb" :alt="strMeal" class="rounded-t-xl h-48 w-full object-cover">
-				</router-link>
-				<div class="p-3">
-					<h3 class="font-semibold">{{ meal.strMeal }}</h3>
-					<p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem cumque placeat illum alias blanditiis possimus.</p>
-					<div class="flex justify-between items-center">
-						<YouTubeButton :href="meal.strYoutube">Youtube</YouTubeButton>
-					</div>
-				</div>
-			</div>
+			<MealItem v-for="meal of meals" :key="meal.idMeal" :meal="meal" />
 		</div>
 </template>
 
 <script setup>
-	import { computed, onMounted, ref } from 'vue';
-	import axiosClient from '../axiosClient';
-	import store from '../store';
+import { computed, onMounted, ref } from 'vue';
+import store from '../store';
 import { useRoute } from 'vue-router';
-import YouTubeButton from '../components/YouTubeButton.vue';
+import MealItem from '../components/MealItem.vue';
 
-	const route = useRoute();
-	const keyword = ref('');
-	const meals = computed(() => store.state.searchedMeals)
+const route = useRoute();
+const keyword = ref('');
+const meals = computed(() => store.state.searchedMeals)
 
-	function searchMeals() {
-		store.dispatch('searchMeals', keyword.value)
+function searchMeals() {
+	store.dispatch('searchMeals', keyword.value)
+}
+
+onMounted(() => {
+	keyword.value = route.params.name
+	if (keyword.value) {
+		searchMeals()
 	}
-
-	onMounted(() => {
-		keyword.value = route.params.name
-		if (keyword.value) {
-			searchMeals()
-		}
-	})
+})
 </script>
